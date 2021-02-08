9aa11cb0a82948f0a1ffd8d606073d1963333d1a
bayzed08 <gbir.islam@gmail.com>
2021-02-09 00:13:27 +0600

    Hard rock API example

---
+++ app.js
@@ -0,0 +1,56 @@
+const searchSong = () => {
+    searchText = document.getElementById('search-field').value;
+    const url = `https://api.lyrics.ovh/suggest/${searchText}`;
+    //load data
+    fetch(url)
+        .then(res => res.json())
+        .then(dataJSON => displaySongs(dataJSON.data))
+        .catch(err => {
+            console.log("err");
+        })
+}
+
+const displaySongs = songs => {
+    const songContainer = document.getElementById('song-container');
+    songContainer.innerHTML = '';
+    songs.forEach(song => {
+        console.log(song);
+       // const songDiv = document.createElement('div');
+        //songDiv.className = 'single-result row align-items-center my-3 p-3';
+        // songDiv.innerHTML = `
+        const preHTML=`
+        <div class="single-result row align-items-center my-3 p-3">
+            <div class="col-md-9">
+               <h3 class="lyrics-name">${song.title}</h3>
+               <p class="author lead">Album by <span>${song.artist.name}</span></p>
+               <audio controls>
+                    <source src="${song.preview}" type="audio/mpeg">
+                    Your browser does not support the audio element.
+                </audio>
+            </div>
+            <div class="col-md-3 text-md-right text-center">
+               <button onclick="displayLyrics('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
+            </div>
+        </div>`;
+        //songContainer.appendChild(songDiv);
+        songContainer.innerHTML = songContainer.innerHTML + preHTML;
+    });
+};
+
+const displayLyrics = (artist, title) => {
+    const songContainer = document.getElementById('song-container');
+    songContainer.innerHTML = '';
+    const displayLyrics = document.getElementById('display-lyrics');
+    displayLyrics.innerHTML = '';
+    const url=`https://api.lyrics.ovh/v1/${artist}/${title}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            const htmlSkeleton = `
+            <div>
+            <p>${data.lyrics}</p>
+            </div>
+            `;
+            displayLyrics.innerHTML = htmlSkeleton;
+        })
+}
